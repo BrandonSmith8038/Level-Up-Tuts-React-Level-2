9596d9bccb4f9629d9620eecc0093f0afdd33ee3
Brandon <brandon@reddirtwebdesign.com>
2018-07-22 15:50:53 -0700

    feat: Using the React Context APi

--- react-projects/src/App.js
+++ react-projects/src/App.js
@@ -1,35 +1,51 @@
-import React, { Component, Fragment } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment, createContext } from 'react';
 import { Modal } from 'Elements';
 import { Toggle } from 'Utilities';
+import { UserContext } from './UserContext';
+import User from './User';
+import logo from './logo.svg';
+import './App.css';
+
+class UserProvider extends Component {
+  state = {
+    id: '123',
+    name: 'Brandon',
+    email: 'Brandon@gmail.com',
+  };
+  render() {
+    return (
+      <UserContext.Provider value={{ user: this.state }}>
+        {this.props.children}
+      </UserContext.Provider>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <h3>Toggle</h3>
-        <Modal>
-          <h1>Still In Modal</h1>
-        </Modal>
-        <Toggle>
-          {({ on, toggle }) => (
-            <Fragment>
-              <button onClick={toggle}>Login</button>
-              <Modal on={on} toggle={toggle}>
-                <h1>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi impedit est magni
-                  similique, molestiae suscipit possimus neque obcaecati cupiditate temporibus.
-                </h1>
-              </Modal>
-            </Fragment>
-          )}
-        </Toggle>
-      </div>
+      <UserProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <User />
+          <Toggle>
+            {({ on, toggle }) => (
+              <Fragment>
+                <button onClick={toggle}>Login</button>
+                <Modal on={on} toggle={toggle}>
+                  <h1>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi impedit est magni
+                    similique, molestiae suscipit possimus neque obcaecati cupiditate temporibus.
+                  </h1>
+                </Modal>
+              </Fragment>
+            )}
+          </Toggle>
+        </div>
+      </UserProvider>
     );
   }
 }
