5ad3bbd49c656765e016f1d5581ae6acbf7ccb1b
Brandon <brandon@reddirtwebdesign.com>
2018-07-22 21:06:55 -0700

    feat: Roating Based on X Postion

--- react-projects/src/Drag.js
+++ react-projects/src/Drag.js
@@ -2,8 +2,21 @@ import React, { Component } from 'react';
 import { Gesture } from 'react-with-gesture';
 import { Spring, animated, interpolate } from 'react-spring';
 import { Card } from 'Elements';
+import styled from 'styled-components';
 
 const AnimCard = Card.withComponent(animated.div);
+
+const DragCard = AnimCard.extend`
+  height: 300px;
+`;
+
+const CardContainer = styled.div`
+  position: relative;
+  background: #ccc;
+  max-width: 320px;
+  margin: 50px auto;
+  border-radius: 5px;
+`;
 export default class Drag extends Component {
   render() {
     return (
@@ -17,13 +30,25 @@ export default class Drag extends Component {
             immediate={name => down && name === 'x'}
           >
             {({ x }) => (
-              <AnimCard
-                style={{
-                  transform: x.interpolate(x => `translateX(${x}px)`),
-                }}
-              >
-                <h1>Drag Me</h1>
-              </AnimCard>
+              <CardContainer>
+                <DragCard
+                  style={{
+                    transform: interpolate(
+                      [
+                        x,
+                        x.interpolate({
+                          range: [-300, 300],
+                          output: [-45, 45],
+                          extrapolate: 'clamp',
+                        }),
+                      ],
+                      (x, rotate) => `translateX(${x}px) rotate(${rotate}deg)`
+                    ),
+                  }}
+                >
+                  <h1>Drag Me</h1>
+                </DragCard>
+              </CardContainer>
             )}
           </Spring>
         )}
